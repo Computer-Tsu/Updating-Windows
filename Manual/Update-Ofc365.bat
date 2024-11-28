a0ff5eec2a13eefb237988a591156d6e5b04661e
Mark McDow <55188587+Computer-Tsu@users.noreply.github.com>
2024-11-28 12:34:37 -0500

    Create Update-Ofc365.bat

---
+++ Manual/Update-Ofc365.bat
@@ -0,0 +1,2 @@
+REM Force manual trigger Microsoft Office 365 Apps click-to-run update/upgrade
+'C:\Program Files\Common Files\microsoft shared\ClickToRun\OfficeC2RClient.exe' /update user displaylevel=false forceappshutdown=true
